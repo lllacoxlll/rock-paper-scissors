76f1d21c12bcda1642537d1cc264269a538d844e
lllacoxlll <65261676+lllacoxlll@users.noreply.github.com>
2025-05-23 18:01:48 -0500

    Add js to script for UI and DOM manipulation

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0
+let computerScore = 0
+
 function getComputerChoice() {
   let randomInt = Math.floor(Math.random() * 3)
   if (randomInt === 0) return 'Rock'
@@ -5,41 +8,64 @@ function getComputerChoice() {
   else if (randomInt === 2) return 'Scissors'
 }
 
-function getHumanChoice() {
-  let userChoice = prompt('Rock, Paper, or Scissors?')
-  return userChoice
-}
-
-function playRound(computerChoice, humanChoice) {
-  humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase()
-
+function playRound(humanChoice, computerChoice) {
   let winningChoice = {
     Rock: 'Scissors',
     Paper: 'Rock',
     Scissors: 'Paper',
   }
 
-  if (computerChoice === humanChoice) console.log('Tie')
+  if (computerChoice === humanChoice) return 'Tie'
   else if (winningChoice[humanChoice] === computerChoice) {
     humanScore++
-    console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+    return `You win! ${humanChoice} beats ${computerChoice}`
   } else {
     computerScore++
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+    return `You lose! ${computerChoice} beats ${humanChoice}`
   }
 }
 
-let humanScore = 0
-let computerScore = 0
-
 function playGame() {
-  for (let i = 0; i < 5; i++) {
-    let humanChoice = getHumanChoice()
-    let computerChoice = getComputerChoice()
-    playRound(computerChoice, humanChoice)
-  }
-  console.log(humanScore)
-  console.log(computerScore)
+  let body = document.querySelector('body')
+
+  let rockBtn = document.createElement('button')
+  const rockText = document.createTextNode('Rock')
+  rockBtn.value = 'Rock'
+  rockBtn.appendChild(rockText)
+  body.appendChild(rockBtn)
+
+  let paperBtn = document.createElement('button')
+  const paperText = document.createTextNode('Paper')
+  paperBtn.value = 'Paper'
+  paperBtn.appendChild(paperText)
+  body.appendChild(paperBtn)
+
+  let scissorsBtn = document.createElement('button')
+  const scissorsText = document.createTextNode('Scissors')
+  scissorsBtn.value = 'Scissors'
+  scissorsBtn.appendChild(scissorsText)
+  body.appendChild(scissorsBtn)
+
+  let newDiv = document.createElement('div')
+  body.appendChild(newDiv)
+
+  let buttons = document.querySelectorAll('button')
+
+  buttons.forEach((button) => {
+    button.addEventListener('click', function toReturn() {
+      newDiv.innerHTML = playRound(button.value, getComputerChoice())
+      newDiv.innerHTML += '<br>Your score: ' + humanScore + " Computer's score: " + computerScore
+      if (humanScore === 5) {
+        newDiv.innerHTML += '<br>You are the winner!'
+      } else if (computerScore === 5) {
+        newDiv.innerHTML += '<br>Sorry, try again next time!'
+      }
+      if (humanScore === 5 || computerScore === 5) {
+        humanScore = 0
+        computerScore = 0
+      }
+    })
+  })
 }
 
 playGame()
